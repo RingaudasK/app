28fdf464fe0d010560b3f4f1657f9c918efbed81
Ringaudas <114056077+RingaudasK@users.noreply.github.com>
2022-12-02 13:42:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.scss';
+
+
+// import Zebrai from './Components/Zebrai.jsx';
+
+
+function App() {
+    
+   
+
+
+
+  return (
+    <div className="App">
+        <div className="App-header">
+        
+          <div>
+            "aaaaa"
+          </div>
+
+        
+ 
+        </div>
+    </div>
+);
+}
+
+ 
+
+ 
+
+ 
+
+export default App;
